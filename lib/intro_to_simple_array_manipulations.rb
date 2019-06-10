ea4892d0bdcf9db0033257f5b5463feb47c67693
HyoSung Bidol-Lee <hyosung11@me.com>
2019-06-10 16:46:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,3 +35,18 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
+  array.flatten
+end
+
+
+  
+  
+
+
+describe "using_flatten" do 
+  it "takes in an argument of an array that contains other arrays and uses the flatten method to return an array of strings" do 
+     instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+     flat_array = using_flatten(instruments)
+     expect(flat_array).to eq(["Saxophone", "Piano", "Trumpet", "Violin", "Drums", "Flute"])
+  end
+end
